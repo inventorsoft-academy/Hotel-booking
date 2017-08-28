d21960cfca00cc376dacef2907f916302b2d39dd
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-08-28 11:15:39 +0300

    create BookingController add registerClient method in BookingController add getRegisterDetails method in BookingController

---
+++ src/main/java/com/bin/otkrivashkin/controller/BookingController.java
@@ -0,0 +1,46 @@
+package com.bin.otkrivashkin.controller;
+
+import com.bin.otkrivashkin.exception.NotEnoughMoneyException;
+import com.bin.otkrivashkin.exception.NotFoundException;
+import com.bin.otkrivashkin.exception.WrongArgumentException;
+import com.bin.otkrivashkin.model.Client;
+import com.bin.otkrivashkin.model.Room;
+import com.bin.otkrivashkin.service.BookingService;
+import com.bin.otkrivashkin.service.ClientService;
+import com.bin.otkrivashkin.service.RoomService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+/**
+ * Created by otkrivashkin on 28.08.2017.
+ */
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/booking")
+public class BookingController {
+
+    private BookingService bookingService;
+    private RoomService roomService;
+    private ClientService clientService;
+
+
+    @PostMapping("/{clientId}/{roomId}/{days}")
+    public void registerClient(@PathVariable("clientId") int clientId,
+                               @PathVariable("roomId") int roomId,
+                               @PathVariable("days") int days) throws NotFoundException, WrongArgumentException, NotEnoughMoneyException {
+        Client clientById = clientService.getClientById(clientId);
+        Room roomById = roomService.getRoomById(roomId);
+        bookingService.registerClient(clientById, roomById, days);
+    }
+
+    @GetMapping
+    public Map<Room, Client> getRegisterDetails() {
+        return bookingService.getRegisterClients();
+    }
+
+
+}
