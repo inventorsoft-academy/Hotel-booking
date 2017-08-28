51ab31f4a366abff510d2298d52885101afe143e
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-08-28 10:59:10 +0300

    add @AllArgsConstructor annotation in SpringBootConfig

--- src/main/java/com/bin/otkrivashkin/config/SpringBootConfig.java
+++ src/main/java/com/bin/otkrivashkin/config/SpringBootConfig.java
@@ -5,18 +5,17 @@ import com.bin.otkrivashkin.service.ClientService;
 import com.bin.otkrivashkin.service.RoomService;
 import com.bin.otkrivashkin.util.FileManager;
 import com.bin.otkrivashkin.util.JsonFileManager;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@AllArgsConstructor
 @Configuration
 public class SpringBootConfig {
 
-    @Autowired
     private RoomService roomService;
-    @Autowired
     private ClientService clientService;
-    @Autowired
     private BookingService bookingService;
 
     @Bean
